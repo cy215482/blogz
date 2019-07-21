53840b2a70e34d837f287e63521e7a9f449200fb
Cat Yokel <cy215482@gmail.com>
2019-07-21 16:32:19 -0500

    betterwork

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def require_login():
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    user = User.query.filter_by(username=username)
+    user = User.query.filter_by(user)
     return render_template('index.html', users = users)
 
 @app.route('/blog', methods=['POST', 'GET'])
