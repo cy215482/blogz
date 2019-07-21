c15a96a754578a020daddfc4c51fb59ac666e5ad
Cat Yokel <cy215482@gmail.com>
2019-07-21 16:30:54 -0500

    gonnaworkorelse

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def require_login():
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    user = User.query.get(user_id)
+    user = User.query.filter_by(username=username)
     return render_template('index.html', users = users)
 
 @app.route('/blog', methods=['POST', 'GET'])
